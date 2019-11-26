e4284132f796d7b8f3fb9a05eadd3601c0d443cd
kkachniarz220 <kkjoonet.pl@onet.pl>
2019-11-26 23:42:57 +0200

    Add message after command

--- src/main/java/com/meme3321/socketcommunication/client/socketcommunicationclient/commands/JoinToArenaCommand.java
+++ src/main/java/com/meme3321/socketcommunication/client/socketcommunicationclient/commands/JoinToArenaCommand.java
@@ -1,7 +1,7 @@
 package com.meme3321.socketcommunication.client.socketcommunicationclient.commands;
 
 import com.meme3321.socketcommunication.client.socketcommunicationclient.executors.SocketExecutor;
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +13,7 @@ public class JoinToArenaCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(cmd.getName().equalsIgnoreCase("dolacz")) {
             Player player = (Player) sender;
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2Dołączanie do areny BedWars..."));
             SocketExecutor.joinToArena(player.getUniqueId(), player.getName());
         }
         return true;
